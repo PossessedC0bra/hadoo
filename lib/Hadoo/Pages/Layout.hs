719c52513b52e65d1f8c037e8606aa071aa2d9bf
Yannick Koller <47781849+PossessedC0bra@users.noreply.github.com>
2022-12-05 21:51:51 +0100

    :lipstick: slightly improve style

--- lib/Hadoo/Pages/Layout.hs
+++ lib/Hadoo/Pages/Layout.hs
@@ -3,4 +3,4 @@ module Hadoo.Pages.Layout where
 import Html
 
 base :: Html -> Html
-base = Html.doc (Html.title "Hadoo" ++ Html.stylesheet "styles.css")
+base = Html.doc (Html.title "Hadoo" ++ Html.stylesheet "styles.css" ++ Html.stylesheet "https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700&&display=swap")
