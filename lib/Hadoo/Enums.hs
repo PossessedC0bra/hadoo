e706cf14cbec57143ced9626e349219c6c091c41
Yannick Koller <47781849+PossessedC0bra@users.noreply.github.com>
2022-12-12 12:07:40 +0100

    :art: adjust state enum definition to readme

--- lib/Hadoo/Enums.hs
+++ lib/Hadoo/Enums.hs
@@ -1,6 +1,6 @@
 module Hadoo.Enums where
 
-data State = Todo | Started | Done deriving (Show, Eq, Bounded, Enum)
+data State = Todo | Started | Done deriving (Show, Read, Eq, Ord, Enum, Bounded)
 
 values :: (Bounded a, Enum a) => [a]
 values = [minBound .. maxBound]
